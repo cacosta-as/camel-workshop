10f16f77b6421f313526b299e7f232bdb016fb8c
Cristian <cmapassert@gmail.com>
2021-08-03 15:15:37 -0500

    Modificado el archivo properties

--- src/main/java/com/assertsl/workshop/beans/FdaEnricher.java
+++ src/main/java/com/assertsl/workshop/beans/FdaEnricher.java
@@ -9,21 +9,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class FdaEnricher implements AggregationStrategy {
 
-    @Override
-    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+	@Override
+	public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
 
-        if (newExchange.getIn().getHeader(Exchange.HTTP_RESPONSE_CODE, Integer.class) == 200) {
-            DrugDto drugDto = oldExchange.getIn().getBody(DrugDto.class);
-            DrugStore drugStore = new DrugStore();
+		if (newExchange.getIn().getHeader(Exchange.HTTP_RESPONSE_CODE, Integer.class) == 200) {
+			DrugDto drugDto = oldExchange.getIn().getBody(DrugDto.class);
+			DrugStore drugStore = new DrugStore();
 
-            drugStore.setProductNdc(drugDto.getProductNdc());
-            drugStore.setExistences(drugDto.getExistences());
-            drugStore.setPrice(drugDto.getPrice());
+			drugStore.setProductNdc(drugDto.getProductNdc());
+			drugStore.setExistences(drugDto.getExistences());
+			drugStore.setPrice(drugDto.getPrice());
 
-            //TODO: set fields packageDescription labelerName genericName returned by the query to drugStore
-
-            newExchange.getIn().setBody(drugStore);
-        }
-        return newExchange;
-    }
+			// TODO: set fields packageDescription labelerName genericName returned by the query to drugStore
+			drugStore.setPackageDescription(newExchange.getIn().getHeader("packageDescription", String.class));
+			drugStore.setGenericName(newExchange.getIn().getHeader("genericName", String.class));
+			drugStore.setLabelerName(newExchange.getIn().getHeader("labelerName", String.class));
+			//			
+			newExchange.getIn().setBody(drugStore);
+		}
+		return newExchange;
+	}
 }
