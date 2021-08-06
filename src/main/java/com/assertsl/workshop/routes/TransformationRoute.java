79fa2d0de2c94cbb3506f61641704bbeb19ab277
Cristian <cmapassert@gmail.com>
2021-08-06 16:00:53 -0500

    modificacion en algunos TODO

--- src/main/java/com/assertsl/workshop/routes/TransformationRoute.java
+++ src/main/java/com/assertsl/workshop/routes/TransformationRoute.java
@@ -78,7 +78,7 @@ public class TransformationRoute extends RouteBuilder {
                 .setHeader("packageDescription", jsonpath("$.results[0].packaging[0].description"))
                 //TODO: get genericName and labelerName fields
                 .setHeader("genericName", jsonpath("$.results[0].generic_name"))
-                .setHeader("labelerName", jsonpath("$results[0].labeler_name"))
+                .setHeader("labelerName", jsonpath("$.results[0].labeler_name"))
                 //                
                 .log("Info obtained packageDescription: ${headers.packageDescription}, labelerName: ${headers.labelerName}, genericName: ${headers.genericName}")
                 .end();
